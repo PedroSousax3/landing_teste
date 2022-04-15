048631b4a495dc51dc9e785f3153e85f3ca2b57f
PedroSousax3 <pedro.sousax3@gmail.com>
2022-04-15 16:52:54 -0300

    Checkin inicial de projeto

---
+++ scripts/router.js
@@ -0,0 +1,20 @@
+function rotear (page) {
+    sessionStorage.setItem('page', page || 'inicio');
+    let path = `${window.location.origin}/pages/${sessionStorage.getItem('page') || 'inicio'}.html`;
+    let request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+        document.getElementById("render-conteudo").innerHTML = this.response;
+    };
+    request.open("GET", path, true);
+    request.send(null);
+}
+
+function lerQueryString() {
+    let resposta = {};
+    location.search.slice(1).split("&").forEach(m => {
+        let resultado = m.split("=");
+        resposta[decodeURIComponent(resultado[0])] = decodeURIComponent(resultado[1]) || '';
+    });
+
+    return resposta;
+}
