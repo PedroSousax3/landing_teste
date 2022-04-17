f4c3601a78a75fafa8baf9ba38c48ac5977fcdc5
PedroSousax3 <pedro.sousax3@gmail.com>
2022-04-17 14:02:36 -0300

    Subindo navegacao dos title

--- scripts/router.js
+++ scripts/router.js
@@ -1,11 +1,11 @@
-function rotear (page, links) {
-    console.log(links);
+function rotear (page, nome) {
     if (!!page)
         sessionStorage.setItem('page', page || 'inicio');
     let path = `${window.location.origin}/pages/${sessionStorage.getItem('page') || 'inicio'}.html`;
     let request = new XMLHttpRequest();
     request.onreadystatechange = function() {
         document.getElementById("render-conteudo").innerHTML = this.response;
+        document.title = nome ?? "Início";
     };
     request.open("GET", path, true);
     request.send(null);
