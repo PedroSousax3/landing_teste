d4f6ec70da4501ec505512ba05f63fa8f60cc1ae
PedroSousax3 <pedro.sousax3@gmail.com>
2022-04-21 18:51:46 -0300

    Adicionar cache

--- scripts/sobre.js
+++ scripts/sobre.js
@@ -3,14 +3,19 @@ function enviarEmail() {
     let txtTelefone = document.getElementById('txtTelefone');
     let txtEmail = document.getElementById('txtEmail');
     let txtObservacao = document.getElementById('txtObservacao');
+    let toast = document.getElementById('liveToast'); 
 
     if (txtNome.value && txtTelefone.value && txtEmail.value)
     {
-        let toast = document.getElementById('liveToast'); 
     
         document.getElementById('dvMensagemEmailEnviado').innerText = `Olá ${txtNome.value.split(' ')[0]}, logo estaremos em contato com o senho(a).`;
         bootstrap.Toast.getOrCreateInstance(toast).show();
     
         txtNome.value = txtTelefone.value = txtEmail.value = txtObservacao.value = '';
+    }
+    else
+    {
+        document.getElementById('dvMensagemEmailEnviado').innerText = `Preencha os campos "Nome", "Telefone" e "E-mail".`;
+        bootstrap.Toast.getOrCreateInstance(toast).show();
     }
 }
