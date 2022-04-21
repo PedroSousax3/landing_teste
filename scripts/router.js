e54ceb42bae385c8a7135c3799d5e7447f95aaed
PedroSousax3 <pedro.sousax3@gmail.com>
2022-04-21 12:13:44 -0300

    Melhorias de layout

--- scripts/router.js
+++ scripts/router.js
@@ -1,12 +1,20 @@
 function rotear (page, nome) {
-    if (!!page)
+    if (!!page) {
         sessionStorage.setItem('page', page || 'inicio');
+        sessionStorage.setItem('name', nome || 'inicio');
+    }
     let path = `${window.location.origin}/pages/${sessionStorage.getItem('page') || 'inicio'}.html`;
     let request = new XMLHttpRequest();
     request.onreadystatechange = function() {
         document.getElementById("render-conteudo").innerHTML = this.response;
-        document.title = nome ?? "Início";
+        document.title = sessionStorage.getItem('name') || "Início";
     };
     request.open("GET", path, true);
     request.send(null);
+}
+
+
+function gerenciarMenu () {
+    let elemento = document.getElementById("nvMenuTop");
+    elemento.style.display = elemento.style.display == 'none' ? 'flex' : 'none';
 }
