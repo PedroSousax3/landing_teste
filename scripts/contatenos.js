b08c71a37dbdb074c2c62cbdb55fad182664f0a1
PedroSousax3 <pedro.sousax3@gmail.com>
2022-04-23 12:39:03 -0300

    Recarreggando arquivos

--- scripts/contatenos.js
+++ scripts/contatenos.js
@@ -1,10 +1,8 @@
-console.log('contatenos');
 function enviarEmail() {
     let txtNome = document.getElementById('txtNome');
     let txtTelefone = document.getElementById('txtTelefone');
     let txtEmail = document.getElementById('txtEmail');
     let txtObservacao = document.getElementById('txtObservacao');
-    let toast = document.getElementById('liveToast'); 
 
     if (txtNome.value && txtTelefone.value && txtEmail.value)
         alert(`Olá ${txtNome.value.split(' ')[0]}, logo estaremos em contato com o senho(a).`);
